24892b3619fdbf3cc1bca4ea86c23d6b2d3d5f6f
stumash <stuart.mashaal@gmail.com>
2019-02-17 00:39:27 -0500

    more useless cleanup

--- code/main_scan.ml
+++ code/main_scan.ml
@@ -1,6 +1,6 @@
 (* main *)
 let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  while true do 
-    Lexer.scanner lexbuf;
-  done
+    let lexbuf = Lexing.from_channel stdin in
+    while true do
+        Lexer.scanner lexbuf
+    done
