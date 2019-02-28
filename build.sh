fc73fbdd001e73c7ff9ddd5d19219a9be10db278
stumash <stuart.mashaal@gmail.com>
2019-02-27 22:22:18 -0500

    update build so that we generate src/_build/parser.conflicts file

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 THIS_DIR="$(dirname "$(readlink -f "$0")")"
 cd "${THIS_DIR}/src"
 
-ocamlbuild -use-menhir main_scan.native
-ocamlbuild -use-menhir main_tokens.native
-ocamlbuild -use-menhir main_parse.native
+ocamlbuild -use-menhir -menhir 'menhir --explain' main_scan.native
+ocamlbuild -use-menhir -menhir 'menhir --explain' main_tokens.native
+ocamlbuild -use-menhir -menhir 'menhir --explain' main_parse.native
