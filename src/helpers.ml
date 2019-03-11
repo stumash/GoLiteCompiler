3532c0773f8ed081447d1bd0b939fc0efa1ed0c4
stumash <stuart.mashaal@gmail.com>
2019-03-11 12:06:46 -0400

    trying to fix error reporting

--- src/helpers.ml
+++ src/helpers.ml
@@ -4,8 +4,9 @@
 
 (* print the compiler stage that detected the error, and its cause *)
 let print_error lexbuf compiler_stage = Lexing.(
-    let {pos_lnum; pos_cnum} = lexbuf.lex_curr_p in
-    Printf.printf "%s Error at L%d,C%d: '%s'\n" compiler_stage pos_lnum pos_cnum (lexeme lexbuf))
+    let {pos_lnum; pos_cnum; pos_bol} = lexbuf.lex_curr_p in
+    let cnum = pos_cnum - pos_bol in
+    Printf.printf "%s Error at L%d,C%d: '%s'\n" compiler_stage pos_lnum cnum (lexeme lexbuf))
 
 (* * 
  * PRETTY PRINTER HELPERS
