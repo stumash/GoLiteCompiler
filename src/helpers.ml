526c53c2db6d87a21341d1f3ec657bc40928c4ad
stumash <stuart.mashaal@gmail.com>
2019-03-12 14:48:09 -0400

    fix merge error - learn how to merge man!

--- src/helpers.ml
+++ src/helpers.ml
@@ -6,27 +6,5 @@
 let print_error lexbuf compiler_stage = Lexing.(
     let {pos_lnum; pos_cnum; pos_bol} = lexbuf.lex_curr_p in
     let cnum = pos_cnum - pos_bol - ((lexeme_end lexbuf) - (lexeme_start lexbuf)) in
-<<<<<<< HEAD
     Printf.printf "Error: '%s' at L%d,C%d: '%s'\n" (lexeme lexbuf) pos_lnum cnum compiler_stage);
     exit 1; ()
-
-(* * 
- * PRETTY PRINTER HELPERS
- *-----------------------*)
-
-(* pretty print a list of xs, comma-separated, using a given pretty printer for type x *)
-let pp_comma_separated_xs xs pp_x =
-    let f i x = pp_x x; if i != (List.length xs)-1 then print_string ", " else () in
-    List.iteri f xs
-
-(* * 
- * GENERAL HELPERS
- *----------------*)
-
-let ifsome o f =
-    match o with
-    | Some a -> f a
-    | _      -> ()
-=======
-    Printf.printf "%s Error at L%d,C%d: '%s'\n" compiler_stage pos_lnum cnum (lexeme lexbuf))
->>>>>>> 15e195f93dcc8733da704d189de0c68f8f7b3ddc
